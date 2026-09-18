83e49d6cac9cf59c5a8f2f992aeb0e1ed7bd0e32
oliver-santos1108 <olivercs1108@gmail.com>
2026-09-18 11:59:52 -0300

    imc1

--- app/src/main/java/com/example/appdesergio/MainActivity.java
+++ app/src/main/java/com/example/appdesergio/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.appdesergio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button b;
     EditText edmin, edmax;
-
+    int posicao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,36 +32,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        b=findViewById(R.id.Button);
-        TextView tv=findViewById(R.id.TextView);
-        edmin= findViewById(R.id.edmin);
-        edmax= findViewById(R.id.edmax);
-
-        b.setOnClickListener(view -> {
-            //contador = (int)(Math.random() *100);
-            String maxst =edmax.getText().toString();
-            String minst =edmin.getText().toString();
-
-            if(minst.isEmpty()){
-                edmin.setError("Informe o valor mínimo.");
-                return;
-            }
-            if(maxst.isEmpty()){
-                edmax.setError("Informe o valor máximo");
-                return;
-            }
-            int min = Integer.parseInt(minst);
-            int max = Integer.parseInt(maxst);
-
-            Random random= new Random();
-            int r = random.nextInt(max-min)+min; //[0,max]
-            tv.setText(Integer.toString(r));
-
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            intent.putExtra("valor",r);
-            startActivity(intent);
-
+        Integer[] imagens = new Integer[]{
+                R.drawable.normal,
+                R.drawable.abaixopeso,
+                R.drawable.obesidade1,
+                R.drawable.obesidade2,
+                R.drawable.obesidade3,
+                R.drawable.sobrepeso
+        };
+        Button botaoVoltar,botaoAvancar;
+        botaoVoltar = findViewById(R.id.btnAnterior);
+        botaoAvancar = findViewById(R.id.btnProximo);
+        ImageView imageView = findViewById(R.id.imageView);
+        botaoAvancar.setOnClickListener(View v ->{
+            posicao++;
+            imageView.setImageResource(imagens)[posicao]);
         });
-    }
+   }
 }
